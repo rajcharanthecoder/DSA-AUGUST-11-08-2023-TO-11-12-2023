4cb0eda91f131922717eb99076779190d68984e7
GANGISETTY RAJ CHARAN <97686664+rajcharanthecoder@users.noreply.github.com>
2023-08-13 17:13:15 +0530

    Create equilibrium index of array.cpp

---
+++ equilibrium index of array.cpp
@@ -0,0 +1,36 @@
+// C++ program to find equilibrium
+// index of an array
+#include <bits/stdc++.h>
+using namespace std;
+
+int equilibrium(int arr[], int n)
+{
+	int i, j;
+	int leftsum, rightsum;
+	for (i = 0; i < n; ++i) {
+
+		/* get left sum */
+		leftsum = 0;
+		for (j = 0; j < i; j++)
+			leftsum += arr[j];
+
+		/* get right sum */
+		rightsum = 0;
+		for (j = i + 1; j < n; j++)
+			rightsum += arr[j];
+
+		if (leftsum == rightsum)
+			return i;
+	}
+
+	return -1;
+}
+
+// Driver code
+int main()
+{
+	int arr[] = { -7, 1, 5, 2, -4, 3, 0 };
+	int arr_size = sizeof(arr) / sizeof(arr[0]);
+	cout << equilibrium(arr, arr_size);
+	return 0;
+}
